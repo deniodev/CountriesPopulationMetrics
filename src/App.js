c54cf89ab747bf5c4945bc28bd5596ae61c9c3b6
Denio Nhanale <dnhanale@gmail.com>
2023-06-29 10:40:09 +0200

    Adding the NavBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import Exercises from './components/Exercises';
+import Details from './components/Details';
 
 function App() {
   return (
-
-    <div>Gym-Exercises</div>
-
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Exercises />} />
+        <Route path="/details" element={<Details />} />
+      </Routes>
+    </div>
   );
 }
 
---
+++ src/components/Exercises.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Exercises = () => (
+  <div>Exercises</div>
+);
+
+export default Exercises;
