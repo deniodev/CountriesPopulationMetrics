e80ac22ee3c8b1b97454386c025227ddaacd182d
Denio Nhanale <dnhanale@gmail.com>
2023-07-01 13:37:09 +0200

    Update the tests

--- src/components/Country.js
+++ src/components/Country.js
@@ -3,33 +3,42 @@ import PropTypes from 'prop-types';
 import { Col } from 'react-bootstrap';
 import styles from './country.module.css';
 
-const Country = (props) => {
-  const { country, detailed } = props;
+const Country = ({ country, detailed }) => {
   const { name, population, flags } = country;
 
   return (
     <Col className={styles.card}>
       <div className={styles.descpDiv}>
-
-        <img className={styles.flag} src={flags.svg} alt={`${name}Country Images`} />
+        {flags && flags.svg && (
+          <img className={styles.flag} src={flags.svg} alt={`${name} Country Images`} />
+        )}
 
         <div className={styles.descp}>
           <h4>{name}</h4>
+          {detailed && (
           <p>
-            {detailed && 'Population: '}
+            Population:
             {population}
           </p>
+          )}
         </div>
-
       </div>
     </Col>
   );
 };
 
 Country.propTypes = {
-  name: PropTypes.string,
-  population: PropTypes.number,
-  flags: PropTypes.any,
-}.isRequired;
+  country: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    population: PropTypes.number.isRequired,
+    flags: PropTypes.shape({
+      svg: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+  detailed: PropTypes.bool,
+};
 
+Country.defaultProps = {
+  detailed: false,
+};
 export default Country;
